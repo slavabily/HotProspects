bdfb6060590e995f7dec9ded07ef58011066d5bd
slavabily <slavabily@me.com>
2020-06-04 18:19:34 +0300

    Understanding Swift’s Result type

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -7,29 +7,58 @@
 //
 
 import SwiftUI
+
+enum NetworkError: Error {
+    case badURL, requestFailed, unknown
+}
  
 struct ContentView: View {
-    @State private var selectedTab = 0
-    
+
     var body: some View {
-        
-        TabView(selection: $selectedTab) {
-            Text("Tab 1")
-                .onTapGesture {
-                    self.selectedTab = 1
+        Text("Hello, World!")
+            .onAppear {
+                self.fetchData(from: "https://www.apple.com") { (result) in
+                    switch result {
+                    case .success(let str):
+                        print(str)
+                    case .failure(let error):
+                        switch error {
+                        case .badURL:
+                            print("BadURL")
+                        case .requestFailed:
+                            print("Network problems")
+                        case .unknown:
+                            print("Unknown error")
+                        }
+                    }
                 }
-                .tabItem {
-                    Image(systemName: "star")
-                    Text("One")
-                }
-                .tag(0)
-            Text("Tab 2")
-                .tabItem {
-                    Image(systemName: "star.fill")
-                    Text("Two")
-                }
-                .tag(1)
+            }
+    }
+    
+    func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void) {
+        // check the URL is OK, otherwize return with a failure
+        guard let url = URL(string: "https://www.apple.com") else {
+            completion(.failure(.badURL))
+            return
         }
+        URLSession.shared.dataTask(with: url) { data, responce, error in
+            // the task is completed - push our work to the main thread
+            DispatchQueue.main.async {
+                if let data = data {
+                    // success: convert the data to a string and send it back
+                    let stringData = String(decoding: data, as: UTF8.self)
+                    completion(.success(stringData))
+                } else if error != nil {
+                    // any sort of network failure
+                    completion(.failure(.requestFailed))
+                } else {
+                    // this ought not to be possible, yet here we are
+                    completion(.failure(.unknown))
+                }
+            }
+        }
+        .resume()
+        
     }
 }
 
