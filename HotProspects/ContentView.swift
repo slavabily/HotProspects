b4ac35c3799e7f19310e5f067b641aad5db1d27a
slavabily <slavabily@me.com>
2020-06-04 20:16:45 +0300

    Manually publishing ObservableObject changes

--- HotProspects/ContentView.swift
+++ HotProspects/ContentView.swift
@@ -7,59 +7,35 @@
 //
 
 import SwiftUI
-
-enum NetworkError: Error {
-    case badURL, requestFailed, unknown
+    
+class DelayedUpdater: ObservableObject {
+    var value = 0 {
+        willSet {
+            objectWillChange.send()
+            
+        }
+    }
+    
+    init() {
+        for i in 1...10 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + Double(i)) {
+                self.value += 1
+            }
+        }
+    }
 }
  
 struct ContentView: View {
+    
+    @ObservedObject var updater = DelayedUpdater()
 
     var body: some View {
-        Text("Hello, World!")
-            .onAppear {
-                self.fetchData(from: "https://www.apple.com") { (result) in
-                    switch result {
-                    case .success(let str):
-                        print(str)
-                    case .failure(let error):
-                        switch error {
-                        case .badURL:
-                            print("BadURL")
-                        case .requestFailed:
-                            print("Network problems")
-                        case .unknown:
-                            print("Unknown error")
-                        }
-                    }
-                }
-            }
+        Text("Value is: \(updater.value)")
+             
     }
     
-    func fetchData(from urlString: String, completion: @escaping (Result<String, NetworkError>) -> Void) {
-        // check the URL is OK, otherwize return with a failure
-        guard let url = URL(string: "https://www.apple.com") else {
-            completion(.failure(.badURL))
-            return
-        }
-        URLSession.shared.dataTask(with: url) { data, responce, error in
-            // the task is completed - push our work to the main thread
-            DispatchQueue.main.async {
-                if let data = data {
-                    // success: convert the data to a string and send it back
-                    let stringData = String(decoding: data, as: UTF8.self)
-                    completion(.success(stringData))
-                } else if error != nil {
-                    // any sort of network failure
-                    completion(.failure(.requestFailed))
-                } else {
-                    // this ought not to be possible, yet here we are
-                    completion(.failure(.unknown))
-                }
-            }
-        }
-        .resume()
-        
-    }
+    
+         
 }
 
 struct ContentView_Previews: PreviewProvider {
